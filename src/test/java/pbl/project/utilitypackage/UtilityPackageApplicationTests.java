7582960e621a4a3f66a0d7adfb798c63d211e016
ParkHyoJin <bhj00702@gmail.com>
2025-03-27 00:52:43 +0900

    프로젝트 생성
    spring boot + react + vite
    빌드시 react도 같이 빌드 및 실행되도록 변경

---
+++ src/test/java/pbl/project/utilitypackage/UtilityPackageApplicationTests.java
@@ -0,0 +1,15 @@
+package pbl.project.utilitypackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityPackageApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
